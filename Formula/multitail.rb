3490cb6792a248cdf3b5d00f93bae9f8b3a48468
Adrian Ho <the.gromgit@gmail.com>
2021-05-17 14:05:42 +0800

    multitail: update 6.5.0 bottle.

--- Formula/multitail.rb
+++ Formula/multitail.rb
@@ -7,7 +7,8 @@ class Multitail < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-new-life/releases/download/multitail-6.5.0"
-    sha256 cellar: :any, big_sur: "63c58d62f56420d226b576d530142471d54067fdb3fc4c71b4889b00e1501449"
+    sha256 cellar: :any, big_sur:  "63c58d62f56420d226b576d530142471d54067fdb3fc4c71b4889b00e1501449"
+    sha256 cellar: :any, catalina: "c6b37d0c8d3217a1fcc4d79a48c555af01209d83b78510f026490274b5fe58e3"
   end
 
   depends_on "pkg-config" => :build
