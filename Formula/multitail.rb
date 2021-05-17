a8c9ae82afd353da07cb871b24d3c3cefe87b9ce
Adrian Ho <the.gromgit@gmail.com>
2021-05-17 13:49:52 +0800

    multitail: update 6.5.0 bottle.

---
+++ Formula/multitail.rb
@@ -0,0 +1,31 @@
+class Multitail < Formula
+  desc "Tail multiple files in one terminal simultaneously"
+  homepage "https://web.archive.org/web/20210322212714/www.vanheusden.com/multitail/"
+  url "https://deb.debian.org/debian/pool/main/m/multitail/multitail_6.5.0.orig.tar.gz"
+  sha256 "b29d5e77dfc663c7500f78da67de5d82d35d9417a4741a89a18ce9ee7bdba9ed"
+  license "GPL-2.0-only"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-new-life/releases/download/multitail-6.5.0"
+    sha256 cellar: :any, big_sur: "63c58d62f56420d226b576d530142471d54067fdb3fc4c71b4889b00e1501449"
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "ncurses"
+
+  def install
+    system "make", "-f", "makefile.macosx", "multitail", "DESTDIR=#{HOMEBREW_PREFIX}"
+
+    bin.install "multitail"
+    man1.install gzip("multitail.1")
+    etc.install "multitail.conf"
+  end
+
+  test do
+    if build.head?
+      assert_match "multitail", shell_output("#{bin}/multitail -h 2>&1", 1)
+    else
+      assert_match version.to_s, shell_output("#{bin}/multitail -h 2>&1", 1)
+    end
+  end
+end
