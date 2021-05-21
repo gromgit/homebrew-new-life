aebd0ee059857cb283ccdbfdc2d5a1319524d6f5
Adrian Ho <the.gromgit@gmail.com>
2021-05-21 13:32:11 +0800

    rsstail: update 2.1 bottle.

--- Formula/rsstail.rb
+++ Formula/rsstail.rb
@@ -14,6 +14,7 @@ class Rsstail < Formula
     root_url "https://github.com/gromgit/homebrew-new-life/releases/download/rsstail-2.1"
     sha256 cellar: :any, big_sur:  "6338fffca791654e11d3c996a47ebe6527830957dbaa210f8fbcdb09904c19e9"
     sha256 cellar: :any, catalina: "7299888957f771fd6835b7999b49135be7bd3ee584d1d8cb06363e2d0ffe3fe1"
+    sha256 cellar: :any, mojave:   "ac2dce4d3128a835284f8a058d0c7a73385ba2a7a78c9798584ff2d0800dd354"
   end
 
   depends_on "libmrss"
