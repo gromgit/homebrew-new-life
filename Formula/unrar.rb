a7cc4f6adea355df8c7cc4d6de0e43b7ef2be75b
Adrian Ho <aho+vm-catalina@03s.net>
2022-03-07 19:02:22 +0800

    unrar: update 6.1.6 bottle.

--- Formula/unrar.rb
+++ Formula/unrar.rb
@@ -14,6 +14,7 @@ class Unrar < Formula
     sha256 cellar: :any, arm64_monterey: "2f6254ec48487d1c93b3ef281f8640ad9c8cea7f2d0abca01a8fefdbb0cd19d1"
     sha256 cellar: :any, monterey:       "f1c9ddb6b88253da148920034b89878e5628df47ab7e5899c11a9a969605cd1f"
     sha256 cellar: :any, big_sur:        "ef51322b8c24a1ddadeb3a6650d28f21915e76dcd4e5cb44958c21d62b014b65"
+    sha256 cellar: :any, catalina:       "286ca40bc896d8aa3318efbe6d8cf634f7c0c4e50a500b9f3d8c0567e7be47e5"
   end
 
   def install
