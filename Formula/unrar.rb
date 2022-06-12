11cd46a844dcd27f482386ed102fbb7840aca111
Adrian Ho <aho+vm-bigsur@03s.net>
2022-06-12 22:09:07 +0800

    unrar: update 6.1.7 bottle.

--- Formula/unrar.rb
+++ Formula/unrar.rb
@@ -12,6 +12,7 @@ class Unrar < Formula
   bottle do
     root_url "https://github.com/gromgit/homebrew-new-life/releases/download/unrar-6.1.7"
     sha256 cellar: :any, arm64_monterey: "37bd4996f319c53180ce7cd7944e23e8629238f97377a8cf74caefe39e12ec53"
+    sha256 cellar: :any, monterey:       "3547dce1a7b27d30bee8a42dc8eb90f3ea544357b54d787ffb03f8b94c6cc62b"
   end
 
   def install
