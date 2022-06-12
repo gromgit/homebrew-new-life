a6a24ad5f5509a0afdd6408adad881f4342fed61
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 22:34:38 +0800

    httping: update 2.5_2 bottle.

--- Formula/httping.rb
+++ Formula/httping.rb
@@ -8,10 +8,11 @@ class Httping < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-new-life/releases/download/httping-2.5"
-    sha256 cellar: :any,                 big_sur:      "d6b14a4d2f6386a0812ee9d8e4fa36dde6c7df9aa96e278ac90668de5c857814"
-    sha256 cellar: :any,                 catalina:     "ba381b9908696c1460e88b59e1b8b09a1cada2021fd2d12cc10a67d838375332"
-    sha256 cellar: :any,                 mojave:       "9fc405def6d134a06bc3e949aad3a118247dd1c199c64c748d2ea68d3414573b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "182067e30ef5120dfb6c92cb0d604d78eaafeb0f66a83dac930b43136c511df9"
+    sha256 cellar: :any,                 arm64_monterey: "2a65c09c6147938f0ef44a99db8534bc1d02a9f87a8ad76cfc09a274f3442d86"
+    sha256 cellar: :any,                 big_sur:        "d6b14a4d2f6386a0812ee9d8e4fa36dde6c7df9aa96e278ac90668de5c857814"
+    sha256 cellar: :any,                 catalina:       "ba381b9908696c1460e88b59e1b8b09a1cada2021fd2d12cc10a67d838375332"
+    sha256 cellar: :any,                 mojave:         "9fc405def6d134a06bc3e949aad3a118247dd1c199c64c748d2ea68d3414573b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "182067e30ef5120dfb6c92cb0d604d78eaafeb0f66a83dac930b43136c511df9"
   end
 
   depends_on "gettext"
