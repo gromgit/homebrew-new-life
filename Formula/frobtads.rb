bc8f65af7aadc8dde4bd43822a53ee71ea0228e4
Adrian Ho <the.gromgit@gmail.com>
2025-02-09 22:54:07 +0800

    add frobtads

---
+++ Formula/frobtads.rb
@@ -0,0 +1,33 @@
+class Frobtads < Formula
+  desc "TADS interpreter and compilers"
+  homepage "https://www.tads.org/frobtads.htm"
+  url "https://github.com/realnc/frobtads/releases/download/v2.0/frobtads-2.0.tar.bz2"
+  sha256 "893bd3fd77dfdc8bfe8a96e8d7bfac693da0e4278871f10fe7faa59cc239a090"
+
+  depends_on "cmake" => :build
+
+  uses_from_macos "curl"
+  uses_from_macos "ncurses"
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"hello.t").write <<~EOS
+      #charset "us-ascii"
+      #include <tads.h>
+
+      main(args) {
+        "Hello, Homebrew!";
+      }
+    EOS
+
+    system bin/"t3make", "hello.t"
+    system bin/"frob", "--no-pause", "hello.t3"
+
+    assert_match version.to_s, shell_output("#{bin}/frob --version")
+  end
+end
