cf91b124afd5eb7cbe05e1be9250754c80b06c00
Adrian Ho <215702+gromgit@users.noreply.github.com>
2025-07-31 12:28:06 +0000

    unrar: update 7.1.10 bottle.

--- Formula/unrar.rb
+++ Formula/unrar.rb
@@ -11,9 +11,9 @@ class Unrar < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/gromgit/new-life"
-    sha256 cellar: :any,                 arm64_sonoma: "59f0fd1b47e1ee0576275197a7928b0705216f455ff5c1f0603aea7c82c447da"
-    sha256 cellar: :any,                 ventura:      "072234f1e0afdf0770763d92d98a90058f8099d3bc6fc50c5a9a3c99c3220b8c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "006255556dfc2475dfba6ef1fe0eb46ae6d370a8e1bddcb6cdb62af8892bb88d"
+    sha256 cellar: :any,                 arm64_sonoma: "a900f4666fd679a706ab1629cf6289ccef274c8d09e973ddd61a66130d472c91"
+    sha256 cellar: :any,                 ventura:      "973a04dedbe858685281968a86926fa905183e8f22f7fe454a9c4790e33e55e0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "72ef5beca11a638b8e171942de93ecc956bfafdb0a1f2629eba91eba9ae44be4"
   end
 
   def install
